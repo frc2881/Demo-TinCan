36aa15e9cc87f740a24c79ce8646b3e79f6de259
Lady Cans <frc2881@gmail.com>
2026-09-21 16:19:10 -0500

    Minor tweaks

--- core/commands/auto.py
+++ core/commands/auto.py
@@ -1,10 +1,6 @@
 from typing import TYPE_CHECKING
-from enum import Enum, auto
 from commands2 import Command, cmd
 if TYPE_CHECKING: from core.robot import RobotCore
-
-class AutoPath(Enum):
-  CUSTOM = auto()
 
 class Auto:
   def __init__(self, robot: "RobotCore") -> None:
